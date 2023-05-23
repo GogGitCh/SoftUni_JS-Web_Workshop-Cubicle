2416d97f74fc6cafb3f188b9abe843bbeb4ae22f
George Dimitrov <georgiangelovdimitrov92@gmail.com>
2023-05-23 17:47:02 +0300

    [add] cube Controller

--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
 const express = require("express");
-const expressConfigurator = require('./config/expressConfig')
-const handlebarsConfigurator = require('./config/handlebars');
-const homeController = require('./controllers/homeController');
-const path = require('path');
+const expressConfigurator = require("./config/expressConfig");
+const handlebarsConfigurator = require("./config/handlebars");
+const homeController = require("./controllers/homeController");
+const cubeController = require("./controllers/cubeController");
+const path = require("path");
 const PORT = 4040;
 
 // Express config
@@ -14,7 +15,8 @@ handlebarsConfigurator(app);
 
 //Routes
 app.use(homeController);
+app.use('/cubes', cubeController);
 
-app.listen(PORT,()=>{
-    console.log(`Express app running on port:${PORT}`);
-})
+app.listen(PORT, () => {
+  console.log(`Express app running on port:${PORT}`);
+});
