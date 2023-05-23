4a5db83391221a9a8f5a0081c65c210c1e84c3db
George Dimitrov <georgiangelovdimitrov92@gmail.com>
2023-05-23 14:42:59 +0300

    [add] static assets

--- src/index.js
+++ src/index.js
@@ -1,1 +1,28 @@
-console.log('Hellow word');
+const path = require('path');
+
+const PORT = 4040;
+
+// Express config
+const express = require("express");
+const app = express();
+app.use(express.static(path.resolve(__dirname, 'public'))) // setting middleware for the static files
+
+// Handlebars config
+const exphbs = require('express-handlebars');
+const handlebars = exphbs.create({
+    extname:'.hbs',
+});
+
+app.engine('.hbs', handlebars.engine);
+app.set('view engine','.hbs');
+app.set('views', 'src/views')
+
+
+//Routes
+app.get("/", (req, res) => {
+  res.render('index');
+});
+
+app.listen(PORT,()=>{
+    console.log(`Express app running on port:${PORT}`);
+})
