3158e833f92f826c5a7247d3e5c4a34f9da91f97
George Dimitrov <georgiangelovdimitrov92@gmail.com>
2023-06-06 18:35:14 +0300

    [add/install] login and register page / bcrypt

---
+++ src/controllers/userController.js
@@ -0,0 +1,17 @@
+const router = require('express').Router();
+
+router.get('/register', (req, res) => {
+    res.render('user/register')
+});
+
+router.get('/login', (req, res) => {
+    res.render('user/login')
+});
+
+
+module.exports = router;
+
+
+
+
+
