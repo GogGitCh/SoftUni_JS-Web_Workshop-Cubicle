bbb5e44aed32985c7465996f1c62ffcb1b0fd618
George Dimitrov <georgiangelovdimitrov92@gmail.com>
2023-06-08 19:58:50 +0300

    [install/add] cookie-parser and jwt / make auth middleware

--- src/config/expressConfig.js
+++ src/config/expressConfig.js
@@ -1,9 +1,13 @@
 const path = require("path");
 const express = require("express");
+const cookieParser = require("cookie-parser");
+const { auth } = require("../middlewares/authmiddleware");
 
 function expressConfigurator(app) {
   app.use(express.static(path.resolve(__dirname, "../public"))); // setting middleware for the static files
-  app.use(express.urlencoded({extended:false}));
+  app.use(express.urlencoded({ extended: false }));
+  app.use(cookieParser());
+  app.use(auth);
 }
 
 module.exports = expressConfigurator;
--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,34 +1,30 @@
-const router = require('express').Router();
-const userManager = require('../services/userManager');
+const router = require("express").Router();
+const userManager = require("../services/userManager");
 
-router.get('/register', (req, res) => {
-    res.render('user/register')
+router.get("/register", (req, res) => {
+  res.render("user/register");
 });
 
-router.post('/register', async  (req, res) => {
-    const { username, password, repassword } = req.body;
+router.post("/register", async (req, res) => {
+  const { username, password, repassword } = req.body;
 
-    await userManager.register({ username, password, repassword });
+  await userManager.register({ username, password, repassword });
 
-    res.redirect('/user/login');
-
+  res.redirect("/user/login");
 });
 
-router.get('/login', (req, res) => {
-    res.render('user/login')
+router.get("/login", (req, res) => {
+  res.render("user/login");
 });
 
-router.post('/login', async (req, res) => {
-    const { username, password} = req.body;
+router.post("/login", async (req, res) => {
+  const { username, password } = req.body;
 
-    const user = await userManager.login(username, password);
+  const token = await userManager.login(username, password);
 
-    res.redirect('/')
+  res.cookie("auth", token, { httpOnly: true });
+
+  res.redirect("/");
 });
 
 module.exports = router;
-
-
-
-
-
